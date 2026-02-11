514cb35fb0ced22880a5c843bbcb18339ce46159
katiejhsu <katiejhsu@gmail.com>
2026-02-11 12:14:39 -0800

    added table view and toolbar

---
+++ iQuiz/iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Katie Hsu on 2/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
